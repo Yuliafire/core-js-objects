6ccfc582a9490698b71a9fedcf89b901e9713f33
Yulia <linguavoice1@yahoo.com>
2025-03-02 11:06:18 +0300

    fix: resctangle function implemented

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -120,7 +120,7 @@ function makeImmutable(obj) {
  * @example
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
- */
+ */ // object.entries() turns and object into an array.
 function makeWord(lettersObject) {
   const result = [];
   Object.entries(lettersObject).forEach(([letter, positions]) => {
@@ -184,8 +184,13 @@ function sellTickets(queue) {
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+
+  Rectangle.prototype.getArea = function () {
+    return this.width * this.height;
+  };
 }
 
 /**
